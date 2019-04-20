96016f30e054a2606b2e131aa0d336f40b524be0
DevashishChoudhary <46700867+DevashishChoudhary@users.noreply.github.com>
2019-04-20 22:07:06 +0530

    Addition of Link_List.

--- file.cpp
+++ file.cpp
@@ -157,3 +157,100 @@ void linklist::addatbeg(int num)
 	q->link = p;
 	p = q;
 }
+void linklist::addafter(int c, int num)
+{
+	node *q, *t; int i;
+
+	for (i = 0, q = p; i < c; i++)
+	{
+		q = q->link;
+		if (q == NULL)
+		{
+			cout << endl << "There are less than " << c << "elements.";
+			return;
+		}
+	}
+	t = new node;
+	t->data = num;
+	t->link = q->link;
+	q->link = t;
+}
+
+void linklist::del(int num)
+{
+	node *q, *r;
+	q = p;
+	if(q->data == num)
+	{
+		p = q->link;
+		delete q;
+		return;
+	}
+
+	r = q;
+	while (q != NULL)
+	{
+		if (q->data == num)
+		{
+			r->link = q->link;
+			delete q;
+			return;
+		}
+		r = q;
+		q = q->link;
+	}
+	cout << endl << "Element " << num << " not found.";
+}
+
+void linklist::display()
+{
+	node *q;
+	cout << endl;
+	for (q = p; q != NULL; q = q->link)
+		cout << endl << q->data;
+}
+
+void linklist::operator=(linklist t)
+{
+	p = t.p;
+}
+
+int linklist::operator==(linklist t)
+{
+	int flag = FALSE;
+	node *a = p, *b = t.p;
+	if (a == NULL && b == NULL)
+	{
+		flag = TRUE;
+		return flag;
+	}
+	while (a != NULL && b != NULL)
+	{
+		if (a->data != b->data)
+			return flag;
+		a = a->link;
+		b = b->link;
+	}
+	flag = TRUE;
+	return flag;
+}
+
+int main()
+{
+	linklist a, b;
+	a.addatbeg(5);
+	a.addatbeg(4);
+	a.addatbeg(3);
+	
+	b = a; //Copying two linked lists.
+	cout <<"linked list : b";
+	b.display();
+	
+	if(a == b)//Comparing two linked lists.
+		cout << "a and b are quivalent.\n";
+	else
+		cout <<"a and b are not equivalent.\n";
+	
+	_getch();
+	return 0;
+}

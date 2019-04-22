1df596e3cba329027e571c6f9883d52cb857cb44
Abhishek Singh <47205003+AbhishekYuvi@users.noreply.github.com>
2019-04-22 22:43:52 +0530

    rotation of the array

--- Searching_Code.cpp
+++ Searching_Code.cpp
@@ -527,3 +527,32 @@ int main() {
         }
     } while (op != 6);
 }
+// left rotation of the array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,d;
+
+	//input value of n and d
+	cout<<"Enter the value of n and d"<<endl;
+	cin>>n>>d;
+	int a[n];
+
+	//input array elements
+	cout<<"enter the array elements : ";
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+
+	//print the elements of array after rotation
+	cout<<"array elements after rotation : ";
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[(i+d)%n]<<" ";
+	}
+
+	return 0;
+}
